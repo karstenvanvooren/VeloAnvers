aa1e45dc5b42801560dd29d333a9877ad77f7cf6
Wim Van Aerschot <wimvanaerschot@Wims-iMac.telenet.be>
2025-03-26 16:19:29 +0100

    initial changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import {useState, useEffect} from 'react';
+import {getDistance} from '@/helpers/get-distance';
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const [filter, setFilter] = useState('');
+  const [location, setLocation] = useState({});
+
+  const response = await fetch('https://api.citybik.es/v2/networks/velo-antwerpen')
+  const data = await response.json()
+  const network = data.network;
+
+  // use effect gebruiken om bv iets op te roepen enkel bij opstart van de app
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
+ 
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error</div>
+
+  const stations = network.stations.filter(station => station.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0);
+
+  // map stations to add disrance to current location
+  stations.map(station => {
+    station.distance = getDistance(location.latitude, location.longitude, station.latitude, station.longitude).distance/1000;
+  });
+
+  // sort stations by distance
+  stations.sort((a, b) => a.distance - b.distance);
+
+  function handleFilterChange(e) {
+    setFilter(e.target.value);
+  }
+  console.log(stations)
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <input type="text" value={filter} onChange={handleFilterChange}/>
+      {stations.map(station => 
+        <div key={station.id}>
+          <Link href={`/stations/${station.id}`}>{station.name}: {station.distance}km</Link>
+        </div>)}
     </div>
-  );
+  )
 }
